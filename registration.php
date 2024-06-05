48c61e3e6aeca45607c362fb0c8626108b28c05f
Blanchou81 <bakakacademic@yahoo.fr>
2024-06-05 14:39:17 +0200

    Debut niveau 2

--- registration.php
+++ registration.php
@@ -49,9 +49,9 @@
                         // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
                         echo "<pre>" . print_r($_POST, 1) . "</pre>";
                         // et complétez le code ci dessous en remplaçant les ???
-                        $new_email = $_POST['???'];
-                        $new_alias = $_POST['???'];
-                        $new_passwd = $_POST['???'];
+                        $new_email = $_POST['email'];
+                        $new_alias = $_POST['pseudo'];
+                        $new_passwd = $_POST['motpasse'];
 
 
                         //Etape 3 : Ouvrir une connexion avec la base de donnée.
